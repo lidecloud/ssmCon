d1680ba40b7e2c91ad52fb02aac885ad9f3da8ce
Luoye <Luoye@qq.com>
2021-12-05 19:29:29 +0800

    hot-fix commit2

--- src/test/java/com/demo/ssm/dao/MapperTest.java
+++ src/test/java/com/demo/ssm/dao/MapperTest.java
@@ -20,5 +20,6 @@ public class MapperTest {
         employeeMapper.insert(new Employee(null, "张三", "m", "123@qq.com", 1));
         System.out.println("success");
         System.out.println("success2");
+        System.out.println("hot-fix");
     }
 }
